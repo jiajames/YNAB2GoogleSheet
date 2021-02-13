a576b3d06a7765f888a31906948fabaa743d844f
James Jia <contact@james-jia.com>
2021-02-12 17:23:44 -0800

    Add 'USER_ENTERED' option to gspread.update. This formats dates and dollar values correctly.

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ def post_gsheets_transactions(auth: AuthData, transaction_sheet: list) -> None:
     ynab_worksheet = sh.worksheet(GSHEET_TXNS_SHEET_NAME)
 
     sheet_range = "A:Z"
-    ynab_worksheet.update(sheet_range, transaction_sheet)
+    ynab_worksheet.update(sheet_range, transaction_sheet, value_input_option='USER_ENTERED')
 
 
 def convert_milliunits_to_dollar_amount(val: int) -> float:
